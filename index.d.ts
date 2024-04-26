b0a626245e1310b3f58cc92ce11475834f68a5a8
abdelhamid Boudjit <abdelhamid.boudjit@gmail.com>
2024-04-26 15:10:05 +0100

    enhance the existing declaration file for SwitchAudioOutput

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,16 @@
 declare namespace SwitchAudioOutput {
-    const getAudioDevices: () => string[]
-    const setAudioDevice: (deviceName: string) => boolean,
-};
+    // Define a union type for possible audio devices
+    export type AudioDevice = "Speaker" | "Bluetooth" | "Headphones";
+
+    // Define a type for the function that gets audio devices
+    export type GetAudioDevices = () => Promise<AudioDevice[]>;
+
+    // Define a type for the function that sets audio device
+    export type SetAudioDevice = (deviceName: AudioDevice) => Promise<boolean>;
+
+    // Export the functions
+    export const getAudioDevices: GetAudioDevices;
+    export const setAudioDevice: SetAudioDevice;
+}
 
 export = SwitchAudioOutput;
